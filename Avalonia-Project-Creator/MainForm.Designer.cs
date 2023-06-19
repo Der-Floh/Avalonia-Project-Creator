587b69384e15fb9c08a1bc506a83b2439d4a69f5
Der_Floh <derr.floh@gmail.com>
2023-06-19 20:17:39 +0200

    Add project files.

---
+++ Avalonia-Project-Creator/MainForm.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Avalonia_Project_Creator;
+
+partial class MainForm
+{
+    /// <summary>
+    ///  Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    ///  Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        ReinstallTemplatesButton = new Button();
+        NewProjectCreateButton = new Button();
+        NewProjectLocationPickButton = new Button();
+        NewProjectLocationLabel = new Label();
+        NewProjectLocationTextBox = new TextBox();
+        NewProjectTypeComboBox = new ComboBox();
+        NewProjectPanel = new Panel();
+        SpinnerLabel = new Label();
+        NewProjectFolderOpenCheckBox = new CheckBox();
+        NewProjectOpenCheckBox = new CheckBox();
+        NewProjectTypeLabel = new Label();
+        NewProjectNameLabel = new Label();
+        NewProjectNameTextBox = new TextBox();
+        NewProjectLabel = new Label();
+        CloseCheckBox = new CheckBox();
+        NewProjectPanel.SuspendLayout();
+        SuspendLayout();
+        // 
+        // ReinstallTemplatesButton
+        // 
+        ReinstallTemplatesButton.Cursor = Cursors.Hand;
+        ReinstallTemplatesButton.Location = new Point(12, 12);
+        ReinstallTemplatesButton.Name = "ReinstallTemplatesButton";
+        ReinstallTemplatesButton.Size = new Size(180, 44);
+        ReinstallTemplatesButton.TabIndex = 0;
+        ReinstallTemplatesButton.Text = "Reinstall Avalonia Templates";
+        ReinstallTemplatesButton.UseVisualStyleBackColor = true;
+        ReinstallTemplatesButton.Click += ReinstallTemplatesButton_Click;
+        // 
+        // NewProjectCreateButton
+        // 
+        NewProjectCreateButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+        NewProjectCreateButton.Cursor = Cursors.Hand;
+        NewProjectCreateButton.Location = new Point(328, 238);
+        NewProjectCreateButton.Name = "NewProjectCreateButton";
+        NewProjectCreateButton.Size = new Size(97, 34);
+        NewProjectCreateButton.TabIndex = 1;
+        NewProjectCreateButton.Text = "Create Project";
+        NewProjectCreateButton.UseVisualStyleBackColor = true;
+        NewProjectCreateButton.Click += NewProjectCreateButton_Click;
+        // 
+        // NewProjectLocationPickButton
+        // 
+        NewProjectLocationPickButton.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        NewProjectLocationPickButton.Cursor = Cursors.Hand;
+        NewProjectLocationPickButton.Location = new Point(360, 106);
+        NewProjectLocationPickButton.Name = "NewProjectLocationPickButton";
+        NewProjectLocationPickButton.Size = new Size(65, 23);
+        NewProjectLocationPickButton.TabIndex = 2;
+        NewProjectLocationPickButton.Text = "Browse";
+        NewProjectLocationPickButton.UseVisualStyleBackColor = true;
+        NewProjectLocationPickButton.Click += NewProjectLocationPickButton_Click;
+        // 
+        // NewProjectLocationLabel
+        // 
+        NewProjectLocationLabel.AutoSize = true;
+        NewProjectLocationLabel.Location = new Point(3, 88);
+        NewProjectLocationLabel.Name = "NewProjectLocationLabel";
+        NewProjectLocationLabel.Size = new Size(103, 15);
+        NewProjectLocationLabel.TabIndex = 3;
+        NewProjectLocationLabel.Text = "Destination Folder";
+        // 
+        // NewProjectLocationTextBox
+        // 
+        NewProjectLocationTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        NewProjectLocationTextBox.Location = new Point(3, 106);
+        NewProjectLocationTextBox.Name = "NewProjectLocationTextBox";
+        NewProjectLocationTextBox.ReadOnly = true;
+        NewProjectLocationTextBox.Size = new Size(351, 23);
+        NewProjectLocationTextBox.TabIndex = 4;
+        // 
+        // NewProjectTypeComboBox
+        // 
+        NewProjectTypeComboBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        NewProjectTypeComboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+        NewProjectTypeComboBox.FormattingEnabled = true;
+        NewProjectTypeComboBox.Items.AddRange(new object[] { "Cross-Platform Application", "Windows Application" });
+        NewProjectTypeComboBox.Location = new Point(3, 166);
+        NewProjectTypeComboBox.Name = "NewProjectTypeComboBox";
+        NewProjectTypeComboBox.Size = new Size(203, 23);
+        NewProjectTypeComboBox.TabIndex = 6;
+        NewProjectTypeComboBox.SelectedIndexChanged += NewProjectTypeComboBox_SelectedIndexChanged;
+        // 
+        // NewProjectPanel
+        // 
+        NewProjectPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        NewProjectPanel.BorderStyle = BorderStyle.FixedSingle;
+        NewProjectPanel.Controls.Add(SpinnerLabel);
+        NewProjectPanel.Controls.Add(NewProjectFolderOpenCheckBox);
+        NewProjectPanel.Controls.Add(NewProjectOpenCheckBox);
+        NewProjectPanel.Controls.Add(NewProjectTypeLabel);
+        NewProjectPanel.Controls.Add(NewProjectTypeComboBox);
+        NewProjectPanel.Controls.Add(NewProjectCreateButton);
+        NewProjectPanel.Controls.Add(NewProjectNameLabel);
+        NewProjectPanel.Controls.Add(NewProjectNameTextBox);
+        NewProjectPanel.Controls.Add(NewProjectLabel);
+        NewProjectPanel.Controls.Add(NewProjectLocationPickButton);
+        NewProjectPanel.Controls.Add(NewProjectLocationTextBox);
+        NewProjectPanel.Controls.Add(NewProjectLocationLabel);
+        NewProjectPanel.Location = new Point(12, 62);
+        NewProjectPanel.Name = "NewProjectPanel";
+        NewProjectPanel.Size = new Size(430, 277);
+        NewProjectPanel.TabIndex = 7;
+        // 
+        // SpinnerLabel
+        // 
+        SpinnerLabel.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+        SpinnerLabel.Location = new Point(229, 248);
+        SpinnerLabel.Name = "SpinnerLabel";
+        SpinnerLabel.Size = new Size(93, 15);
+        SpinnerLabel.TabIndex = 13;
+        SpinnerLabel.Text = "\\";
+        SpinnerLabel.TextAlign = ContentAlignment.MiddleRight;
+        SpinnerLabel.Visible = false;
+        // 
+        // NewProjectFolderOpenCheckBox
+        // 
+        NewProjectFolderOpenCheckBox.AutoSize = true;
+        NewProjectFolderOpenCheckBox.Cursor = Cursors.Hand;
+        NewProjectFolderOpenCheckBox.Location = new Point(5, 234);
+        NewProjectFolderOpenCheckBox.Name = "NewProjectFolderOpenCheckBox";
+        NewProjectFolderOpenCheckBox.Size = new Size(166, 19);
+        NewProjectFolderOpenCheckBox.TabIndex = 12;
+        NewProjectFolderOpenCheckBox.Text = "Open Folder after Creation";
+        NewProjectFolderOpenCheckBox.UseVisualStyleBackColor = true;
+        NewProjectFolderOpenCheckBox.CheckedChanged += NewProjectFolderOpenCheckBox_CheckedChanged;
+        // 
+        // NewProjectOpenCheckBox
+        // 
+        NewProjectOpenCheckBox.AutoSize = true;
+        NewProjectOpenCheckBox.Cursor = Cursors.Hand;
+        NewProjectOpenCheckBox.Location = new Point(5, 209);
+        NewProjectOpenCheckBox.Name = "NewProjectOpenCheckBox";
+        NewProjectOpenCheckBox.Size = new Size(170, 19);
+        NewProjectOpenCheckBox.TabIndex = 8;
+        NewProjectOpenCheckBox.Text = "Open Project after Creation";
+        NewProjectOpenCheckBox.UseVisualStyleBackColor = true;
+        NewProjectOpenCheckBox.CheckedChanged += NewProjectOpenCheckBox_CheckedChanged;
+        // 
+        // NewProjectTypeLabel
+        // 
+        NewProjectTypeLabel.AutoSize = true;
+        NewProjectTypeLabel.Location = new Point(3, 148);
+        NewProjectTypeLabel.Name = "NewProjectTypeLabel";
+        NewProjectTypeLabel.Size = new Size(71, 15);
+        NewProjectTypeLabel.TabIndex = 11;
+        NewProjectTypeLabel.Text = "Project Type";
+        // 
+        // NewProjectNameLabel
+        // 
+        NewProjectNameLabel.AutoSize = true;
+        NewProjectNameLabel.Location = new Point(3, 30);
+        NewProjectNameLabel.Name = "NewProjectNameLabel";
+        NewProjectNameLabel.Size = new Size(39, 15);
+        NewProjectNameLabel.TabIndex = 10;
+        NewProjectNameLabel.Text = "Name";
+        // 
+        // NewProjectNameTextBox
+        // 
+        NewProjectNameTextBox.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+        NewProjectNameTextBox.Location = new Point(3, 48);
+        NewProjectNameTextBox.Name = "NewProjectNameTextBox";
+        NewProjectNameTextBox.Size = new Size(282, 23);
+        NewProjectNameTextBox.TabIndex = 9;
+        NewProjectNameTextBox.TextChanged += NewProjectNameTextBox_TextChanged;
+        // 
+        // NewProjectLabel
+        // 
+        NewProjectLabel.AutoSize = true;
+        NewProjectLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+        NewProjectLabel.Location = new Point(3, 2);
+        NewProjectLabel.Name = "NewProjectLabel";
+        NewProjectLabel.Size = new Size(76, 15);
+        NewProjectLabel.TabIndex = 8;
+        NewProjectLabel.Text = "New Project";
+        // 
+        // CloseCheckBox
+        // 
+        CloseCheckBox.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+        CloseCheckBox.AutoSize = true;
+        CloseCheckBox.Cursor = Cursors.Hand;
+        CloseCheckBox.Location = new Point(312, 12);
+        CloseCheckBox.Name = "CloseCheckBox";
+        CloseCheckBox.Size = new Size(130, 19);
+        CloseCheckBox.TabIndex = 13;
+        CloseCheckBox.Text = "Close after Creation";
+        CloseCheckBox.UseVisualStyleBackColor = true;
+        CloseCheckBox.CheckedChanged += CloseCheckBox_CheckedChanged;
+        // 
+        // MainForm
+        // 
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(454, 351);
+        Controls.Add(CloseCheckBox);
+        Controls.Add(NewProjectPanel);
+        Controls.Add(ReinstallTemplatesButton);
+        MinimumSize = new Size(470, 390);
+        Name = "MainForm";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "Avalonia Project Creator";
+        Load += MainForm_Load;
+        NewProjectPanel.ResumeLayout(false);
+        NewProjectPanel.PerformLayout();
+        ResumeLayout(false);
+        PerformLayout();
+    }
+
+    #endregion
+
+    private Button ReinstallTemplatesButton;
+    private Button NewProjectCreateButton;
+    private Button NewProjectLocationPickButton;
+    private Label NewProjectLocationLabel;
+    private TextBox NewProjectLocationTextBox;
+    private ComboBox NewProjectTypeComboBox;
+    private Panel NewProjectPanel;
+    private Label NewProjectLabel;
+    private Label NewProjectNameLabel;
+    private TextBox NewProjectNameTextBox;
+    private Label NewProjectTypeLabel;
+    private CheckBox NewProjectOpenCheckBox;
+    private CheckBox NewProjectFolderOpenCheckBox;
+    private CheckBox CloseCheckBox;
+    private Label SpinnerLabel;
+}
---
+++ Avalonia-Project-Creator/MainForm.cs
@@ -0,0 +1,325 @@
+﻿using System.Diagnostics;
+
+namespace Avalonia_Project_Creator;
+
+public partial class MainForm : Form
+{
+    private string ProjectName
+    {
+        get => _projectName;
+        set
+        {
+            _projectName = value;
+            if (NewProjectNameTextBox.Text != value)
+                NewProjectNameTextBox.Text = value;
+            Properties.Settings.Default.ProjectName = value;
+            Properties.Settings.Default.Save();
+
+            NewProjectCreateButton.Enabled = !string.IsNullOrEmpty(ProjectName);
+        }
+    }
+    private string _projectName;
+    private string ProjectPath
+    {
+        get => _projectPath;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                value = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            _projectPath = value;
+            if (NewProjectLocationTextBox.Text != value)
+                NewProjectLocationTextBox.Text = value;
+            Properties.Settings.Default.ProjectPath = value;
+            Properties.Settings.Default.Save();
+
+            NewProjectCreateButton.Enabled = !string.IsNullOrEmpty(ProjectPath);
+        }
+    }
+    private string _projectPath;
+    private int ProjectType
+    {
+        get => _projectType;
+        set
+        {
+            _projectType = value;
+            if (NewProjectTypeComboBox.SelectedIndex != value)
+                NewProjectTypeComboBox.SelectedIndex = value;
+            Properties.Settings.Default.ProjectType = value;
+            Properties.Settings.Default.Save();
+        }
+    }
+    private int _projectType;
+    private bool ProjectOpen
+    {
+        get => _projectOpen;
+        set
+        {
+            _projectOpen = value;
+            if (NewProjectOpenCheckBox.Checked != value)
+                NewProjectOpenCheckBox.Checked = value;
+            Properties.Settings.Default.ProjectOpen = value;
+            Properties.Settings.Default.Save();
+        }
+    }
+    private bool _projectOpen;
+    private bool ProjectFolderOpen
+    {
+        get => _projectFolderOpen;
+        set
+        {
+            _projectFolderOpen = value;
+            if (NewProjectFolderOpenCheckBox.Checked != value)
+                NewProjectFolderOpenCheckBox.Checked = value;
+            Properties.Settings.Default.ProjectFolderOpen = value;
+            Properties.Settings.Default.Save();
+        }
+    }
+    private bool _projectFolderOpen;
+    private bool AppClose
+    {
+        get => _appClose;
+        set
+        {
+            _appClose = value;
+            if (CloseCheckBox.Checked != value)
+                CloseCheckBox.Checked = value;
+            Properties.Settings.Default.AppClose = value;
+            Properties.Settings.Default.Save();
+        }
+    }
+    private bool _appClose;
+    private System.Threading.Timer _timer;
+    private string _spinners = "◜◠◝◞◡◟";
+    private int spinnerIndex = 0;
+
+    public MainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void _timer_Tick(object? state)
+    {
+        SpinnerLabel.BeginInvoke(new Action(() =>
+        {
+            SpinnerLabel.Text = _spinners[spinnerIndex].ToString();
+            spinnerIndex++;
+            if (spinnerIndex >= _spinners.Length)
+                spinnerIndex = 0;
+        }));
+    }
+
+    private async Task<bool> ReinstallTemplates()
+    {
+        await ExecuteCommand("dotnet new uninstall Avalonia.Templates");
+        bool success = await ExecuteCommand("dotnet new install Avalonia.Templates");
+        return success;
+    }
+
+    private async Task<bool> CreateWindowsProject(string name, string path)
+    {
+        return await ExecuteCommand("dotnet new avalonia.app -o " + name, path);
+    }
+
+    private async Task<bool> CreateCrossPlatformProject(string name, string path)
+    {
+        path = Path.Combine(path, name);
+        Directory.CreateDirectory(path);
+        return await ExecuteCommand("dotnet new avalonia.xplat", path);
+    }
+
+    private string? SelectFolder(string? initialDirectory = null)
+    {
+        using FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+        if (!string.IsNullOrEmpty(initialDirectory))
+            folderBrowserDialog.SelectedPath = initialDirectory;
+        DialogResult result = folderBrowserDialog.ShowDialog();
+
+        if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+            return folderBrowserDialog.SelectedPath;
+        return null;
+    }
+
+    private async Task<bool> ExecuteCommand(string command, string? workingDirectory = null)
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            Arguments = "/c " + command,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+        if (!string.IsNullOrEmpty(workingDirectory))
+            startInfo.WorkingDirectory = workingDirectory;
+
+        using Process process = new Process { StartInfo = startInfo };
+        process.Start();
+
+        string output = process.StandardOutput.ReadToEnd();
+        string error = process.StandardError.ReadToEnd();
+
+        await process.WaitForExitAsync();
+
+        return process.ExitCode == 0;
+    }
+
+    private bool OpenProject(string pName, string pPath)
+    {
+        string path = Path.Combine(pPath, pName, pName + ".sln");
+        if (File.Exists(path))
+        {
+            try
+            {
+                Process.Start("explorer", "\"" + path + "\"");
+                return true;
+            }
+            catch
+            {
+                DialogResult result = MessageBox.Show("Failed to open Project '" + pName + "'. Do you want to open the Folder instead?", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+                if (result == DialogResult.Yes)
+                    return OpenProjectFolder(pName, pPath);
+                else
+                    return false;
+            }
+        }
+        else
+        {
+            DialogResult result = MessageBox.Show("Failed to open Project '" + pName + "'. '" + pName + ".sln' doesn't exist. Do you want to open the Folder instead?", "Error", MessageBoxButtons.YesNo, MessageBoxIcon.Error);
+            if (result == DialogResult.Yes)
+                return OpenProjectFolder(pName, pPath);
+            else
+                return false;
+        }
+    }
+
+    private bool OpenProjectFolder(string pName, string pPath)
+    {
+        string path = Path.Combine(pPath, pName);
+        if (Directory.Exists(path))
+        {
+            try
+            {
+                Process.Start("explorer", "\"" + path + "\"");
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Failed to open Folder '" + pName + "'", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+        else
+            MessageBox.Show("Failed to open Folder '" + pName + "'. Folder doesn't exist.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        return false;
+    }
+
+    private async Task<bool> HandleProjectCreation()
+    {
+        StartSpinner();
+        bool success = false;
+        switch (ProjectType)
+        {
+            case 0:
+                success = await CreateCrossPlatformProject(ProjectName, ProjectPath);
+                break;
+            case 1:
+                success = await CreateWindowsProject(ProjectName, ProjectPath);
+                break;
+        }
+
+        if (success)
+        {
+            if (ProjectOpen)
+                OpenProject(ProjectName, ProjectPath);
+            else if (ProjectFolderOpen)
+                OpenProjectFolder(ProjectName, ProjectPath);
+        }
+
+        StopSpinner();
+        SpinnerLabel.BeginInvoke(new Action(() =>
+        {
+            if (success)
+            {
+                SpinnerLabel.ForeColor = Color.Green;
+                SpinnerLabel.Text = "success";
+            }
+            else
+            {
+                SpinnerLabel.ForeColor = Color.Red;
+                SpinnerLabel.Text = "failed";
+            }
+            SpinnerLabel.Visible = true;
+        }));
+        if (AppClose)
+            BeginInvoke(new Action(Close));
+        return success;
+    }
+
+    private void StartSpinner()
+    {
+        SpinnerLabel.BeginInvoke(new Action(() =>
+        {
+            SpinnerLabel.ForeColor = SystemColors.ControlText;
+            SpinnerLabel.Text = _spinners[0].ToString();
+            SpinnerLabel.Visible = true;
+        }));
+        _timer = new System.Threading.Timer(_timer_Tick, null, 0, 100);
+    }
+
+    private void StopSpinner()
+    {
+        SpinnerLabel.BeginInvoke(new Action(() => SpinnerLabel.Visible = false));
+        _timer.Dispose();
+    }
+
+    private void MainForm_Load(object sender, EventArgs e)
+    {
+        ProjectName = Properties.Settings.Default.ProjectName;
+        ProjectPath = Properties.Settings.Default.ProjectPath;
+        ProjectType = Properties.Settings.Default.ProjectType;
+        ProjectOpen = Properties.Settings.Default.ProjectOpen;
+        ProjectFolderOpen = Properties.Settings.Default.ProjectFolderOpen;
+        AppClose = Properties.Settings.Default.AppClose;
+    }
+
+    private void NewProjectNameTextBox_TextChanged(object sender, EventArgs e)
+    {
+        ProjectName = NewProjectNameTextBox.Text;
+    }
+
+    private void NewProjectLocationPickButton_Click(object sender, EventArgs e)
+    {
+        ProjectPath = SelectFolder(Properties.Settings.Default.ProjectPath);
+    }
+
+    private void NewProjectTypeComboBox_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        ProjectType = NewProjectTypeComboBox.SelectedIndex;
+    }
+
+    private void NewProjectOpenCheckBox_CheckedChanged(object sender, EventArgs e)
+    {
+        ProjectOpen = NewProjectOpenCheckBox.Checked;
+    }
+
+    private void NewProjectFolderOpenCheckBox_CheckedChanged(object sender, EventArgs e)
+    {
+        ProjectFolderOpen = NewProjectFolderOpenCheckBox.Checked;
+    }
+
+    private void CloseCheckBox_CheckedChanged(object sender, EventArgs e)
+    {
+        AppClose = CloseCheckBox.Checked;
+    }
+
+    private void NewProjectCreateButton_Click(object sender, EventArgs e)
+    {
+        Task.Run(HandleProjectCreation);
+    }
+
+    private void ReinstallTemplatesButton_Click(object sender, EventArgs e)
+    {
+        ReinstallTemplates();
+    }
+}
